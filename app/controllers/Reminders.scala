21f3969c100ca8d83f9db222006277264a188eb7
Willard Korfhage <willard.korfhage@ntkn.com>
2012-04-01 18:36:31 -0500

    Trying to figure out how to handle multiselect

--- app/controllers/Reminders.scala
+++ app/controllers/Reminders.scala
@@ -9,10 +9,58 @@ import anorm._
 
 import models.Reminder
 
-case class ReminderForm(reminder: Reminder, people: List[Long]);
+case class ReminderAndPeople(reminder: Reminder, people: List[Long]);
 
 object Reminders extends Controller {
   
+	/*
+
+
+1) Use the attached multi select input helper
+2) You'll need to remap the incoming form data before binding it to the underlying Form object. You could use this Java code as a guideline to bind to the Form (instead of using the standard Form<T>.bindFromRequest):
+
+	Map<String, String> newData = new HashMap<String, String>();
+	Map<String, String[]> urlFormEncoded = play.mvc.Controller.request().body().asFormUrlEncoded();
+	if (urlFormEncoded != null) {
+		for (String key : urlFormEncoded.keySet()) {
+			String[] value = urlFormEncoded.get(key);
+			if (value.length == 1) {
+				newData.put(key, value[0]);
+			} else if (value.length > 1) {
+				for (int i = 0; i < value.length; i++) {
+					newData.put(key + "[" + i + "]", value[i]);
+				}
+			}
+		}
+	}
+	// bind to the MyEntity form object
+	Form<MyEntity> saveForm = new Form<MyEntity>(MyEntity.class).bind(newData);
+*/
+/*
+	def reminderAndPeopleForm = Form(
+		mapping (
+			"id" -> ignored(NotAssigned:Pk[Long]),
+			"reminder_date" -> date,
+			"contract_id" -> longNumber,
+			"sent" -> boolean,
+			"people" -> List[longNumber]
+		)
+		(
+			(id, reminder_date, contract_id, sent, people) =>
+				ReminderAndPeople(Reminder(id, new LocalDate(reminder_date), contract_id, sent), List(people))
+		)
+		(
+			(reminder: Reminder) => Some ((
+				reminder.id,
+				reminder.reminderDate.toDate,
+				reminder.contractId,
+				reminder.sent
+			))
+		)
+	)
+	*/
+
+
 	def reminderForm = Form(
 		/*tuple (
 			"reminder_date" -> date,
@@ -43,6 +91,7 @@ object Reminders extends Controller {
 	}
 
 	def create = Action { implicit request =>
+		println(request.body)
 		reminderForm.bindFromRequest.fold(
 			formWithErrors => BadRequest(views.html.reminder.list(Reminder.all(), formWithErrors)),
 			reminder => {
