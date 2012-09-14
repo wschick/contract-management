31c4882bf3cd78f0acd1dcc804330d72697fea53
Willard Korfhage <willard@ntkn>
2012-09-14 14:41:45 -0500

    Yet another build version bump to keep everything consistent.

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "contracts"
-    val appVersion      = "1.0.2"
+    val appVersion      = "1.0.3"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
